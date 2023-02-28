37ce4ccd4d7308f72e480079f1ff83fc9a786955
stefvtls <stefvtls@gmail.com>
2023-02-28 13:05:26 +0100

    REFACTOR: refactored browser session storage method

--- src/app/utils/storage.utils.ts
+++ src/app/utils/storage.utils.ts
@@ -1,11 +1,11 @@
 export class StorageUtil {
 
-    public static localStorageSave<T>(key: string, value: any): void {
+    public static sessionStorageSave<T>(key: string, value: any): void {
         console.log(key, value)
         sessionStorage.setItem(key, JSON.stringify(value));
     }
     
-    public static localStorageRead<T>(key: string): T | undefined {
+    public static sessionStorageRead<T>(key: string): T | undefined {
         const storedValue = sessionStorage.getItem(key);
         try {
           if (storedValue) {
